39af05030f12aee7aac943e35077f1c0d8c4ec29
Medy <krouk.medy@gmail.com>
2022-10-24 15:27:17 +0200

    First Commit.

---
+++ todolistproject/src/App.vue
@@ -0,0 +1,18 @@
+ <template>
+   <div><todos>
+
+   </todos></div>
+
+</template>
+
+<script>
+import Todos from './components/Todos.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Todos
+  }
+}
+</script>
+
---
+++ todolistproject/src/components/Todos.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "Todos.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
