8f88364a6f9713ae751b8ce678e97de829b188bf
Medy <krouk.medy@gmail.com>
2022-10-31 12:13:27 +0100

    first commit.

---
+++ weather_app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<div id="app">
+  <weather-app></weather-app>
+</div>
+</template>
+
+<script>
+
+
+
+
+import WeatherApp from "@/components/WeatherApp";
+
+export default {
+  name: 'App',
+
+  components: {
+    WeatherApp
+  }
+}
+</script>
+
+
+
+
+<style>
+#app{
+
+}
+
+</style>
---
+++ weather_app/src/components/WeatherApp.vue
@@ -0,0 +1,139 @@
+<template>
+  <main>
+    <div class="search-box">
+      <input
+          type="text"
+          class="search-bar"
+          name="search"
+          id="search"
+          placeholder="Choisir un lieu"
+          v-model="query"
+          @keypress="fetchApiWeather"
+      />
+    </div>
+
+    <div class="weather-wrap">
+      <div class="location-box">
+        <div class="location">{{ weather.name }}</div>
+        <div class="date">Monday 31 octobre 2022</div>
+      </div>
+
+      <div class="weather-box">
+        <div class="temp">22°C</div>
+        <div class="weather">Soleil</div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "WeatherApp",
+  data() {
+    return {
+      api_key: '3612570726d94981b3ebe6af117e0346',
+      url_base: 'api.openweathermap.org/data/2.5/',
+      query: '',
+      weather: {}
+    }
+  },
+  methods: {
+    fetchApiWeather(e) {
+      if (e.key === "Enter") {
+        fetch(`${this.url_base}forecast?q=${this.query}&appid=${this.api_key}`)
+        .then(response => {
+          return response.json();
+        }).then(this.setResults);
+      }
+    },
+    setResults(results){
+      this.weather = results;
+    }
+
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+main {
+
+  font-family: Roboto, sans-serif;
+  background-image: url("/src/assets/weather_images.jpeg");
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+  padding: 25px;
+  min-height: 100vh;
+
+
+}
+
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+
+}
+
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  font-size: 20px;
+
+  appearance: none;
+  border: none;
+  outline: none;
+  border-radius: 5px;
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+}
+
+.location-box .location {
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 1px rgba(0, 0, 0, 0.25);
+}
+
+.location-box .date {
+  font-size: 22px;
+  font-weight: 300;
+  text-align: center;
+  text-shadow: 1px 1px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box {
+  text-align: center;
+}
+
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: black;
+  font-size: 100px;
+  font-weight: 900;
+  text-shadow: 1px 1px rgba(0, 0, 0, 0.25);
+
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px;
+  margin: 30px 0px;
+
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.15);
+}
+
+.weather-box .weather {
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 1px rgba(0, 0, 0, 0.25);
+}
+
+</style>
